172424b4b43ab192e6a1c8484566e2a0d3083665
Bruno <brunoferreira.tecnico@gmail.com>
2025-02-16 23:13:45 -0300

    first project configuration

---
+++ src/test/java/com/ambev/project_order/ProjectOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambev.project_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
